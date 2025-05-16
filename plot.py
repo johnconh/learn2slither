c5d38c83c18aff7af047b90bc8ab3577ff1dbcf3
johnconh <sevenballs@gmail.com>
2025-05-16 20:15:54 +0200

    plot norminette

--- plot.py
+++ plot.py
@@ -3,7 +3,8 @@ from IPython import display
 
 plt.ion()
 
-def plot (scores, mean_scores):
+
+def plot(scores, mean_scores):
     display.clear_output(wait=True)
     display.display(plt.gcf())
     plt.clf()
