ca25760fef5f343f672719d6e8e6abfed2531727
johnconh <sevenballs@gmail.com>
2025-05-20 18:55:15 +0200

    block size

--- snake_game.py
+++ snake_game.py
@@ -103,12 +103,13 @@ class SnakeGame:
     def reset(self):
         self.direction = Direction.RIGHT
         self.head = Point(
-            (self.w // 2) // self.block_size * self.block_size,
-            (self.h // 2) // self.block_size * self.block_size
+            (self.w // 2) // BLOCK_SIZE * BLOCK_SIZE,
+            (self.h // 2) // BLOCK_SIZE * BLOCK_SIZE
         )
         self.snake = [self.head,
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y),
+                    ]
         self.score = 0
         self.green_apples = []
         self.red_apple = None
